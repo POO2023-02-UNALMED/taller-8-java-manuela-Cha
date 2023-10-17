ebe18584cccd929879c3178e0ab1e23c9d63f510
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-10-17 00:11:05 -0500

    Portero.java

---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,47 @@
+package futbol;
+
+public class Portero extends Futbolista{
+
+	public short golesRecibidos;
+	public byte dorsal;
+	
+	public Portero() {
+		super("Maradona",30,"Portero");
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Portero(String name, int age, short point, byte dorsal) {
+		super(name, age,"Portero" );
+		this.golesRecibidos=point;
+		this.dorsal=dorsal;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public short getGolesRecibidos() {
+		return golesRecibidos;
+	}
+	
+	public void setGolesRecibidos(short golesRecibidos) {
+		this.golesRecibidos = golesRecibidos;
+	}
+	
+	public byte getDorsal() {
+		return dorsal;
+	}
+	
+	public void setDorsal(byte dorsal) {
+		this.dorsal = dorsal;
+	}
+	
+	 String toString() {
+		 return super.toString()+" con el dorsal"+this.dorsal+". Le han marcado "+this.golesRecibidos;
+    }
+	 
+	 public int compareTo(Object obj) {
+		        Portero otroPortero = (Portero) obj;
+		        return Math.abs(this.getGolesRecibidos() - otroPortero.getGolesRecibidos());}	    
+	 public  boolean jugarConLasManos(){
+		 return true;
+	 }
+
+}
